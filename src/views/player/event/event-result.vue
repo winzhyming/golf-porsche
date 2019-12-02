e11ad8597571f28b639183c053a32bbd60f50bea
winzhyming <winzhyming@163.com>
2019-12-02 09:46:36 +0800

    update score result

--- src/views/player/event/event-result.vue
+++ src/views/player/event/event-result.vue
@@ -9,20 +9,28 @@
           <th>奖项</th>
           <th>组别</th>
           <th>姓名</th>
-          <th>码数/分数</th>
+          <th>所属经销商</th>
         </thead>
         <tbody>
           <tr v-for="(da, index) in list" :key="index">
             <td v-html="da.cate"></td>
-            <td>{{ da.name }}</td>
+            <!-- <td>{{ da.name }}</td>
             <td>{{ da.ys}}</td>
-            <td>{{ da.domain }}</td>
+            <td>{{ da.domain }}</td> -->
+            <td>{{ da.group }}</td>
+            <td>{{ da.name }}</td>
+            <td>{{ da.score }}</td>
           </tr>
-          <!-- <tr>
+          <tr>
             <td>{{ team.cate }}</td>
             <td colspan="2">{{ team.name }}</td>
-            <td>{{ team.domain }}</td>
-          </tr> -->
+            <td>{{ team.score }}</td>
+          </tr>
+          <tr>
+            <td colspan="2">{{ best.cate }}</td>
+            <td>{{ best.name }}</td>
+            <td>{{ best.score }}</td>
+          </tr>
         </tbody>
       </table>
     </div>
@@ -39,102 +47,106 @@
       return {
         list: [
           {
-            cate: '男子距线最近奖',
-            group: '男子组',
-            name: '',
-            score: ''
-          },
-          {
             cate: '女子最远距离奖',
             group: '女子组',
-            name: '',
-            score: ''
-          },
-          {
-            cate: '男子距线最近奖',
-            group: '男子组',
-            name: '',
-            score: ''
+            name: '武晓庆',
+            score: '215 码',
+            domain: '深圳南山 &  宝安保时捷中心'
           },
           {
             cate: '女子距线最近奖',
             group: '女子组',
-            name: '',
-            score: ''
+            name: '李淑萍',
+            score: '2 码',
+            domain: '佛山顺德保时捷中心'
           },
+          {
+            cate: '男子最远距离奖',
+            group: '男子组',
+            name: '赵光明',
+            score: '300 码',
+            domain: '太原保时捷中心'
+          },
+          {
+            cate: '男子距线最近奖',
+            group: '男子组',
+            name: '沈亮',
+            score: '0.1 码',
+            domain: '上海闵行保时捷中心'
+          },
+
           {
             cate: '距洞最近奖',
             group: '',
-            name: '',
-            score: ''
+            name: '廖志辉',
+            score: '5 码',
+            domain: '佛山顺德保时捷中心'
           },
           {
             cate: '两杆最近洞奖',
             group: '',
-            name: '',
-            score: ''
+            name: '谭键明',
+            score: '16 码',
+            domain: '佛山南海保时捷中心'
           },
-          {
-            cate: '一杆进洞',
-            group: '',
-            name: '',
-            score: ''
-          },
-          {
-            cate: '最佳团队',
-            group: '',
-            name: '',
-            score: ''
-          },
-          {
-            cate: '保时捷俱乐部最佳表现奖',
-            group: '',
-            name: '',
-            score: ''
-          },
+
           {
             cate: '净杆 <span class="fen">C</span> 组冠军',
             group: '',
-            name: '',
-            score: ''
+            name: '杨关进',
+            score: '27 分',
+            domain: '惠州保时捷中心'
           },
           {
-            cate: '净杆 <span class="fen">B</span> 组冠军',
+            cate: '净杆 <span class="fen">B</span> 组冠军   ',
             group: '',
-            name: '',
-            score: ''
+            name: '曾子鹏',
+            score: '35 分',
+            domain: '佛山顺德保时捷中心'
           },
           {
             cate: '净杆 <span class="fen">A</span> 组冠军',
             group: '',
-            name: '',
-            score: ''
+            name: '孙开',
+            score: '36 分',
+            domain: '北京金港保时捷中心'
           },
+
           {
             cate: '总杆季军',
             group: '',
-            name: '',
-            score: ''
+            name: '谭键明',
+            score: '83 杆',
+            domain: '佛山南海保时捷中心'
           },
           {
             cate: '总杆亚军',
             group: '',
-            name: '',
-            score: ''
+            name: '任泳潮',
+            score: '81（后九 42 杆）',
+            domain: '深圳龙岗 & 福田保时捷中心'
           },
           {
             cate: '总杆冠军',
             group: '',
-            name: '',
-            score: ''
+            name: '叶伟欣',
+            score: '81杆（后九 40 杆）',
+            domain: '深圳龙岗 & 福田保时捷中心'
           }
         ],
         team: {
           cate: '最佳团队',
           group: '',
-          name: '吕世双、郭金龙、单继红',
-          ys: '两个最高得分之和 76 分',
+          name: '孙开、吴雨东、黄鹏',
+          score: '68分',
           domain: '北京金港保时捷中心'
+        },
+        best: {
+          cate: '保时捷俱乐部最佳表现奖',
+          group: '',
+          name: '沈亮',
+          score: '6杆',
+          domain: '上海闵行保时捷中心'
         }
       }
     }
